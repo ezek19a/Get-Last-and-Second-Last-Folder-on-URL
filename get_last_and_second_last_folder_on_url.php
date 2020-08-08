fea3f54ec8ad8dae7d6b4001e49fcd2e0cb98477
ezek19a <43878810+ezek19a@users.noreply.github.com>
2020-08-08 13:20:16 +0800

    Create get_last_and_second_last_folder_on_url.php

---
+++ get_last_and_second_last_folder_on_url.php
@@ -0,0 +1,18 @@
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+$full = explode('/', $actual_link);
+$pop = array_pop($full);
+$fullnlastfolder = implode('/', $full);
+$secondfull = explode('/', $fullnlastfolder);
+$secondpop = array_pop($secondfull);
+$secondlast = implode('/', $secondfull);
+$realpath = $fullnlastfolder;
+$secondpath = $secondlast;
+
+define('LAST', $realpath);
+
+define('SECOND', $secondpath);
+
+echo "LAST: " . LAST;
+
+echo "SECOND: " . SECOND;
